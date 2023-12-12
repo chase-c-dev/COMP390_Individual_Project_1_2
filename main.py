f202c3c65c53382976af8d8e3ff0157ab6ae7f69
Chase <76849972+chase-c-dev@users.noreply.github.com>
2023-12-12 12:05:58 -0800

    Started Added DocStrings
    Added DocStrings to the main file and terminal text file

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from exelfile_create import *
 # Project 1.2 For Software Engineering
 
 def inputmenu():
+    '''
+    This is the docstring for the input menu function. 
+    This function runs all the functions that generate the user prompts
+
+    Parameters:
+    - There are no parameters for this function
+
+    Returns:
+    - This function does not return
+    '''
     fileEntry = FileDataEntry("", "", "", "", "")
     # welcome message
     welcome_message()
@@ -22,9 +32,31 @@ def inputmenu():
     choose_output_result_type(fileEntry)
 
 def file_path_prompt(fileEntry):
+    '''
+    This is the docstring for the file path prompt function. 
+    This function runs the function that generates the prompt for the input file
+
+    Parameters:
+    - This function takes fileEntry as a parameter because 
+      the function it runs utilize this object
+
+    Returns:
+    - This function does not return
+    '''
     file_path_data_prompter(fileEntry, "Enter a valid file name(ex. filename.txt) with its file extension (if applicable) or enter >q or >Q to quit", "Target file: ")
 
 def mode_prompt(fileEntry):
+    '''
+    This is the docstring for the mode prompt function. 
+    This function runs the function that generates the prompt for the mode
+
+    Parameters:
+    - This function takes fileEntry as a parameter because 
+      the function it runs utilize this object 
+
+    Returns:
+    - This function does not return
+    '''
     file_mode_data_prompter(fileEntry, 'What mode would you like to open up the file with\n' 
         '"r" - open for reading (default)\n'
         '"m" - open for writing, truncating the file first (WARNING: this mode will delete the contents of an existing file)\n'
@@ -37,6 +69,19 @@ def mode_prompt(fileEntry):
 
 
 def filterfile(fileEntry):
+    '''
+    This is the docstring for the filter file function. 
+    This function opens up the input file and stores the filtered meteor entries in a list
+    After doing this tableCreate is ran with the filtered meteorite entry list
+
+    Parameters:
+    - This function takes fileEntry as a parameter because 
+      the functions it runs utilize this object, this object is also
+      used to get the file path and mode to open the file
+
+    Returns:
+    - This function does not return
+    '''
     final_list = [] # stores the sorted meteor data entries
     with open(fileEntry.textfile, fileEntry.mode) as f:
         next(f) # skips first line of text in txt file
@@ -46,11 +91,37 @@ def filterfile(fileEntry):
 
 
 def tableCreate(final_list, fileEntry): # creates the tables in the terminal
+    '''
+    This is the docstring for the table create function. 
+    This function creates the table that will be printed to the terminal
+
+    Parameters:
+    - This function takes fileEntry as a parameter because 
+      the functions it runs utilize this object 
+
+    - This function takes final_list as a parameter because this stores all the
+      filtered meteorsite entries that can be used to generate the table
+
+    Returns:
+    - This function does not return
+    '''
     indexholder = parseIndex(fileEntry)
     printTable(final_list, fileEntry, indexholder) # prints out table into terminal
 
 
 def choose_output_result_type(fileEntry): # prompts user for options to output file data
+    '''
+    This is the docstring for choosing the output type function. 
+    This function runs the functions based off user input that either print to terminal,
+    write to a text file or generate an excel file
+
+    Parameters:
+    - This function takes fileEntry as a parameter because 
+      the functions it runs utilize this object 
+
+    Returns:
+    - This function does not return
+    '''
     output_type = output_result_type_prompt("", fileEntry)
     filterfile(fileEntry)
     if int(output_type) == 2:
--- terminaltext.py
+++ terminaltext.py
@@ -10,6 +10,16 @@ all functions involve printing to the terminal and collecting user input
 
 # prints out welcome message
 def welcome_message(): 
+    '''
+    This is the docstring for the welcome message function. 
+    This function prints out the welcome message to the program
+
+    Parameters:
+    - This function has no parameters
+
+    Returns:
+    - This function does not return
+    '''
     print("Welcome to the meteorite filtering program\n") # welcome message
     print("This program allows you to filter NASA meteorite data contained in a text file\n")
     print("The data in the text file must be organized in a specific format\n Required format:\n    Each line of the text file describes a single meteorite.")
@@ -18,28 +28,115 @@ def welcome_message():
     print("The release date of this program is October 5th 2023\n")
 
 def file_mode_data_prompter(fileEntry, input_text, output_text): # runs the input menu and the quitCheck function for mode and file name
+    '''
+    This is the docstring for the mode prompter function
+    This function sets the mode equal to what the user inputs and
+    then runs a function that checks for quit or continue
+
+    Parameters:
+    - fileEntry is an object that stores the mode amongst other things
+
+    - input_text contains the user prompt string
+
+    - output_text contains the string which attached to the mode will let the user know what they inputted
+
+    Returns:
+    - This function does not return
+    '''
     fileEntry.mode = input(input_text)
     quitCheckMode(fileEntry.mode, output_text, fileEntry) # checks to see if the program exits or continues
 
 def file_path_data_prompter(fileEntry, input_text, output_text): # runs the input menu and the quitCheck function for mode and file name
+    '''
+    This is the docstring for the file path prompt function. 
+    This function sets file path equal to user input
+
+    Parameters:
+    - fileEntry is an object that stores the text file path amongst other things
+
+    - input_text is the string that contains the prompt for file path
+
+    - output_text contains the string that when attached to the file path lets the user know
+      what they inputted 
+
+    Returns:
+    - This function does not return
+    '''
     fileEntry.textfile = input(input_text)
     quitCheckFilePath(fileEntry.textfile, output_text, fileEntry) # checks to see if the program exits or continues
 
 def file_bounds_prompter(file_entry, input_text, output_text): # runs the input menu and quit check for filter bounds
+    '''
+    This is the docstring for the file bounds prompt function. 
+    This function returns the bound the user inputted
+
+    Parameters:
+    - file_entry is the bound portion of the object fileEntry
+
+    - input_text contains the string for the user prompt
+
+    - output_text contains the string that when attached to the inputted bound lets the user
+      know what they inputted
+
+    Returns:
+    - This function returns the bound the user inputted
+    '''
     file_entry = input(input_text)
     quitCheckBounds(file_entry, output_text) # checks to see if the program exits or continues
     return file_entry
 
 def exit_check(promptInput): # checks to see if program should exit
+    '''
+    This is the docstring for the exit check function. 
+    This function checks to see if the program exits or not
+    It does this by checking if the user input is >q or >Q
+
+    Parameters:
+    - promptInput contains the user input for the prompt 
+
+    Returns:
+    - This function does not return
+    '''
     if promptInput == ">q" or promptInput == ">Q": 
         print("The program is now exiting Goodbye!")
         exit() # exits program
 
 def quitCheckFilePath(promptInput, output_text, fileEntry): # checks file path to see if program should quit or if theres an error
+    '''
+    This is the docstring for the quit file path prompt function. 
+    This function checks to see if the program quits during the file path prompt and runs
+    the error check function
+
+    Parameters:
+    - promptInput contains the user input for the prompt
+
+    - output_text contains the string that when attached to the user input is printed to let
+      the user know what they inputted
+
+    - fileEntry is an object that is used in other functions ran by this function 
+
+    Returns:
+    - This function does not return
+    '''
     exit_check(promptInput)
     errorcheck(output_text, 1, fileEntry)
 
 def quitCheckMode(promptInput, output_text, fileEntry): # checks mode to see if program should quit or if theres an error
+    '''
+    This is the docstring for the quit check mode function. 
+    This function checks if the program quits and runs the error checker
+
+    Parameters:
+    - fileEntry is an object used in functions this function runs 
+
+    - promptInput contains the mode inputted by the user
+
+    - output_text contains the string that attaches 
+      to the mode string to let the user know what they inputted 
+
+    Returns:
+    - This function does not return
+    '''
     exit_check(promptInput)
     errorcheck(output_text, 2, fileEntry)
        
@@ -56,6 +153,14 @@ def errorcheck(output_text, num, fileEntry): # runs error checks for various use
         file_path_data_prompter(fileEntry, "Enter a valid file name(ex. filename.txt) with its file extension (if applicable) or enter >q or >Q to quit", "Target file: ")
     if num == 2 and check_for_mode(fileEntry, output_text) == False:
         file_mode_data_prompter(fileEntry, mode_prompt_text(), "File Mode: ")
+    if (num == 3 or num == 4 or num == 5):
+        errorcheck_bounds(output_text, num, fileEntry)
+    if num == 6 and check_display_type(output_text, fileEntry) == False:
+        output_result_type_prompt(output_text, fileEntry)
+    else:
+        pass
+
+def errorcheck_bounds(output_text, num, fileEntry): # runs error checks for bounds and category
     if num == 3 and filter_category_check(output_text) == False:
         dataFiltering(fileEntry)
     if num == 4 and check_lower_bound(fileEntry, output_text) == False:
@@ -64,10 +169,7 @@ def errorcheck(output_text, num, fileEntry): # runs error checks for various use
     if num == 5 and check_upper_bound(fileEntry, output_text) == False:
         fileEntry.upper_bound = file_bounds_prompter(fileEntry.upper_bound, "Enter the UPPER limit (inclusive) for the meteor's " + output_text + " , (Enter Q to quit) \n", "Upper Bound: ")
         errorcheck(output_text, 5, fileEntry)
-    if num == 6 and check_display_type(output_text, fileEntry) == False:
-        output_result_type_prompt(output_text, fileEntry)
-    else:
-        pass
+    else: pass
 
 def mode_prompt_text(): # returns string for mode prompt
     mode_prompt = 'What mode would you like to open up the file with\n' 
